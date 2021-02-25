294803f145afd973aab8de5472e4cc317ae0f3b2
EliveltonSouzaDev <tst.elivelton@gmail.com>
2021-02-24 21:41:27 -0300

    papel de parede

--- main.js
+++ main.js
@@ -2,10 +2,10 @@
 
 function enviar(fundo) {
 switch (fundo) {
-    case 'naruto':document.body.style.background = "url('../jogo/img/fundonaruto.jpg')";
+    case 'naruto':document.body.style.background = "url('https://images2.alphacoders.com/109/thumb-1920-1096941.png')";
     break
 
-    case 'sasuke':document.body.style.background = "url('https://wallpaperaccess.com/full/262868.jpg')";
+    case 'sasuke':document.body.style.background = "url('https://images5.alphacoders.com/100/thumb-1920-1009469.jpg')";
     break
 
     case 'sakura':document.body.style.background = "url('https://cdn.wallpapersafari.com/14/80/StN0mL.jpg')";
